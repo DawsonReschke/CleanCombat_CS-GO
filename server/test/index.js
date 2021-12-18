3d72353e51e4ece639389b08405dd6b2d0afcdeb
Dawson <dawsonreschke@icloud.com>
2021-12-18 11:56:16 -0800

    continuing testing API calls and recording the response formats, soon to begin creating the backend server, as well as the front end code...

--- server/test/index.js
+++ server/test/index.js
@@ -34,9 +34,14 @@ const steamConvertTo64Int = 76561197960265728n
 const steamID = 'STEAM_1:1:126461176'
 const key = 'FE57479D48DDA7694ABA7FC43ECC83E8'
 let uData;
+let userSteamLevel;
 let userStats; 
+let userBanRecord; 
+let userGamesOwnedData; 
 
-
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// get summary data
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
 /*
 RESPONSE FORMAT: 
 response: {
@@ -62,20 +67,29 @@ response: {
 }
 */
 
-
  async function getUserData (steamid){
-    // this method takes a users steam id
-//https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={key}&steamids=76561197960361544
     const URL = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2'
-    let temp = fetch(`${URL}/?key=${key}&steamids=${steamIdToSteam64(steamID)}`).then(response => response.json()).then(data => uData = data).catch(error => uData = 'error');
+    let temp = fetch(`${URL}/?key=${key}&steamids=${steamIdToSteam64(steamid)}`).then(response => response.json()).then(data => uData = data['response']['players'][0]).catch(error => uData = 'error');
     //USEFULL DATA: avatar, date created, communityvisibilitystate
 }
 
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// get ingame stats
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+
+/*
+    RESPONSE FORMAT: 
+    playerstats:{
+        steamID:76561198325083676,
+        gameName:ValveTestApp260,
+        stats: [{name:total_kills,value:51237},,,[187]]
+    }
+*/
+
 function getUserStatsFromGame(steamid){
     const URL = 'https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/'
-    let sID = '76561198325083676'
     const appID = '000730'
-    let temp = fetch(`${URL}/?key=${key}&steamid=${sID}&appid=${appID}`).then(response => response.json()).then(data => userStats = data); 
+    let temp = fetch(`${URL}/?key=${key}&steamid=${steamIdToSteam64(steamid)}&appid=${appID}`).then(response => response.json()).then(data => userStats = data); 
     // NOW: parse the data, get all relative information, including: 
     //USEFULL DATA : 
     //kills, HEADSHOTS, wins,losses, shots hit, shots fired, 
@@ -84,15 +98,71 @@ function getUserStatsFromGame(steamid){
     // ACHIEVMENT: count the number, compare it to the max, only sus if 100%
 }
 
-// async function getServerSteamIDsByIP(ip) {
-//     const URL = 'https://api.steampowered.com/IGameServersService/GetServerSteamIDsByIP/v1'
-//     let temp = fetch(`${URL}/?key=${key}&server_ips=${ip}`).then(response => response.json()).then(data => steamidList = data)
-// }
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// get banned information... 
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
 
-// async function getServerIPsBySteamID(steamids) {
-//     const URL = 'https://api.steampowered.com/IGameServersService/GetServerIPsBySteamID/v1'
-//     let temp = fetch(`${URL}/?key=${key}&server_steamids=${steamids}`).then(response => response.json()).then(data => serveripList = data);
-// }
+/*
+    RESPONSE FORMAT: 
+    players:{
+        [
+            {
+                "SteamId": "76561198213188081",
+                "CommunityBanned": false,
+                "VACBanned": false,
+                "NumberOfVACBans": 0,
+                "DaysSinceLastBan": 1394,
+                "NumberOfGameBans": 1,
+                "EconomyBan": "none"
+            },
+        ]   
+    }
+*/
+
+function getUserBanRecord(steamids){
+    const URL = 'https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/'
+    let temp = fetch(`${URL}?key=${key}&steamids=${steamIdToSteam64(steamids)}`).then(response => response.json()).then(data => userBanRecord = data); 
+}
+
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+// get steam level
+//««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+
+/*
+    RESPONSE FORMAT:
+    response: { 
+        "player_level": 9
+    }
+*/
+
+function getUserSteamLevel(steamid){
+    const URL = 'https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/'
+    let temp = fetch(`${URL}?key=${key}&steamid=${steamIdToSteam64(steamid)}`).then(response => response.json()).then(data => userSteamLevel = data['response']); 
+}
+
+/*
+    RESPONSE: 
+    resonse:{
+        game_count: 92,
+        games: 
+        [
+            {
+                "appid": 4000,
+                "playtime_forever": 5785,
+                "playtime_windows_forever": 1346,
+                "playtime_mac_forever": 0,
+                "playtime_linux_forever": 0
+            },
+        ]
+    }
+*/
+//GET https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/
+function getUserHoursPlayed(steamid){
+    const URL = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/'
+    let temp = fetch(`${URL}?key=${key}&steamid=${steamIdToSteam64(steamid)}&include_appinfo=false&include_played_free_games=true&appids_filter=000730`).then(response => response.json()).then(data => userGamesOwnedData = data['response']['games'].filter((val) => {
+        return val['appid'] == 730; 
+    })); 
+}
 
 
 //this method is neccessary for converting from steamID to steam64ID so that we can pass in steam64 into api calls...
@@ -108,7 +178,14 @@ function steamIdToSteam64(str){
 
 getUserData(steamID);
 getUserStatsFromGame(steamID);
+getUserSteamLevel(steamID); 
+getUserBanRecord(steamID); 
+getUserHoursPlayed(steamID); 
 setTimeout(() => {
     console.log(JSON.stringify(uData, null, 4));
-    console.log(JSON.stringify(userStats,null,4));
+    console.log(JSON.stringify(userSteamLevel,null,4)); 
+    console.log(JSON.stringify(userBanRecord,null,4)); 
+    console.log(JSON.stringify(userGamesOwnedData,null,4));
+    console.log(JSON.stringify(userStats['playerstats']['achievements'].length));
+    console.log(JSON.stringify(userStats['playerstats']['stats'].length));
 }, 1000);
